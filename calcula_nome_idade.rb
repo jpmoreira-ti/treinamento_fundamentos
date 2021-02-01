cc5bb63e276e227437776ae6eb36b809b07204e7
joao.moreira <joao.moreira@neon.com.br>
2021-02-01 20:31:38 -0300

    Exercicio Aula_002

---
+++ calcula_nome_idade.rb
@@ -0,0 +1,7 @@
+print "Digite o nome da pessoa: "
+nome = gets.chomp.to_s
+ 
+print "Digite a idade da pessoa: "
+idade = gets.chomp.to_i
+ 
+puts "Olá #{nome.upcase} . Você tem #{idade} anos de idade :)"
